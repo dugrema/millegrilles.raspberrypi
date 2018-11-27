0514af2311562d569bec85ff28aa08339bf94093
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-27 09:48:15 -0500

    Fix repertoires pour script demarrage appareils

--- scripts/mg-appareils.sh
+++ scripts/mg-appareils.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-FICHIER_CONFIG=../etc/mg-appareils.conf
-#FICHIER_CONFIG=/usr/local/etc/millegrilles/mg-appareils.conf
-SCRIPT_PYTHON=/home/mathieu/git/MilleGrilles.raspberrypi/scripts/mgraspberrypi.py
+FICHIER_CONFIG=/usr/local/etc/mg-appareils.conf
+SCRIPT_PYTHON=/usr/local/bin/mgraspberrypi.py
 COMMAND=$1
 
 if [ ! -f $FICHIER_CONFIG ]; then
@@ -12,21 +11,6 @@ fi
 
 source $FICHIER_CONFIG
 
-#export MG_NOM_MILLEGRILLE=maple
-#export MG_MQ_HOST=127.0.1.1
-#export MG_MQ_USER=garage
-#export MG_MQ_PASSWORD=feIT5558idRO
-#export MG_MONGO_HOST=127.0.1.1
-#export MG_MONGO_SSL=nocert
-#export MG_MONGO_USER=garage
-#export MG_MONGO_PASSWORD=valHONK331ro
-
-#SENSEUR_NO=25
-#SENSEUR_PIN=24
-
-#GARAGE_NO_SENSEUR=25
-#GARAGE_PIN=24
-
 PARAMS=""
 
 # Ajouter parametre pour senseur AM2302 au besoin
@@ -35,4 +19,4 @@ if [ ! -z GARAGE_NO_SENSUER ]; then
 fi
 
 echo "Commande: $SCRIPT_PYTHON $PARAMS $COMMAND"
-#$SCRIPT_PYTHON $PARAMS $COMMAND
+$SCRIPT_PYTHON $PARAMS $COMMAND
