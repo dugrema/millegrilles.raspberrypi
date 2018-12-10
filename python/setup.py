926fbd9f619495da5c31090425ab4254f14ad282
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-10 12:26:24 -0500

    Update a version 0.9.0.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrillesRPi',
-    version='v0.8.0',
+    version='v0.9.0',
     packages=[
         'mgraspberry',
         'mgraspberry.raspberrypi'
