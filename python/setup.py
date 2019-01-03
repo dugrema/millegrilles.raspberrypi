32f05039f8655de24321ecb89995b16a319ad7ee
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-02 22:49:38 -0500

    v1.3.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrillesRPi',
-    version='v0.10.2',
+    version='v1.3.0',
     packages=[
         'mgraspberry',
         'mgraspberry.raspberrypi'
