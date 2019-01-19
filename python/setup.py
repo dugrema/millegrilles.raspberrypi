1cc2411a980550345adde2e03ccab13bfc5364c6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-19 15:41:32 -0500

    Update info version 1.4.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrillesRPi',
-    version='v1.3.6',
+    version='v1.4.0',
     packages=[
         'mgraspberry',
         'mgraspberry.raspberrypi'
