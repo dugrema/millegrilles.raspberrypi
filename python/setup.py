27b6e8135efc6984d19a189da8f4b20848eeeb33
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-06-15 09:28:25 -0400

    Creation branch 1.6.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrillesRPi',
-    version='v1.5.0',
+    version='v1.6.0',
     packages=[
         'mgraspberry',
         'mgraspberry.raspberrypi'
