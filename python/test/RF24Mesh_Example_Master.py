47dd40090cc7df9d3d4c548d94a066bb51806904
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-08 18:27:51 +0000

    Ajout classe pour traiter messages par type.

--- python/test/RF24Mesh_Example_Master.py
+++ python/test/RF24Mesh_Example_Master.py
@@ -6,8 +6,8 @@ from RF24Mesh import *
 
 from struct import unpack
 
-import uuid
 import binascii
+import datetime
 
 
 # radio setup for RPi B Rev2: CS0=Pin 24
@@ -21,75 +21,173 @@ mesh.begin(87)
 radio.setPALevel(RF24_PA_LOW) # Power Amplifier
 radio.printDetails()
 
-commande_precedente = None
+
+reception_par_nodeId = dict()
 
 
-def lire_th(data):
+class Paquet:
 
-    th_values = unpack('hH', data[4:8])
-    temperature = th_values[0]
-    humidite = th_values[1]
+    def __init__(self, data: bytes):
+        self.__data = data
 
-    if temperature == -32768:
-        temperature = None
-    else:
-        temperature = float(temperature) / 10.0
-    if humidite == 0xFF:
-        humidite = None
-    else:
-        humidite = float(humidite) / 10.0
+        self.version = None
+        self.type_message = None
+        self.nombrePaquets = None
 
-    print('Temp: {}, Humidite: {}'.format(temperature, humidite))
+        self._parse()
 
+    def _parse(self):
+        self.version = self.data[0]
+        self.type_message = self.data[1:3]
+        self.nombrePaquets = unpack('h', self.data[3:5])
 
-def lire_tp(data):
-
-    th_values = unpack('hH', data[4:8])
-    temperature = th_values[0]
-    pression = th_values[1]
-
-    if temperature == -32768:
-        temperature = None
-    else:
-        temperature = float(temperature) / 10.0
-    if pression == 0xFF:
-        pression = None
-    else:
-        pression = float(pression) / 100.0
-
-    print('Temp: {}, Pression: {}'.format(temperature, pression))
+    @property
+    def data(self):
+        return self.__data
 
 
-def lire_power(data):
+class Paquet0(Paquet):
 
-    th_values = unpack('IBB', data[4:10])
-    millivolt = th_values[0]
-    reserve = th_values[1]
-    alerte = th_values[2]
+    def __init__(self, data: bytes):
+        super().__init__(data)
+        self.uuid = None
 
-    if millivolt == 0xFFFFFFFF:
-        millivolt = None
-    if reserve == 0xFF:
-        reserve = None
+    def _parse(self):
+        super()._parse()
+        self.uuid = binascii.hexlify(self.data[5:22]).decode('utf-8')
 
-    print('Millivolt: {}, Reserve: {}, Alerte: {}'.format(millivolt, reserve, alerte))
-
-
-def lire_mv(data):
-    mv_values = unpack('LLLL', data[4:20])
-    print("Millivolt: {}, {}, {}, {}".format(mv_values[0], mv_values[1], mv_values[2], mv_values[3]))
+    def __str__(self):
+        return 'Paquet0 UUID: %s, type: %s, nombrePaquets: %s' % (self.uuid, hex(self.type_message), self.nombrePaquets)
 
 
-def routage_type_message(data):
-    commande = unpack('H', data[2:4])[0]
-    if commande == 0x102:
-        lire_th(data)
-    elif commande == 0x103:
-        lire_tp(data)
-    elif commande == 0x104:
-        lire_power(data)
-    else:
-        raise ValueError("Type message inconnu %s" % hex(commande))
+class PaquetPayload(Paquet):
+
+    def __init__(self, data: bytes):
+        super().__init__(data)
+        self.__noPaquet = None
+
+    def _parse(self):
+        super()._parse()
+
+    @property
+    def noPaquet(self):
+        return self.__noPaquet
+
+
+class PaquetTP(PaquetPayload):
+    def __init__(self, data: bytes):
+        super().__init__(data)
+        self.temperature = None
+        self.pression = None
+
+    def _parse(self):
+        super()._parse()
+        th_values = unpack('hH', self.data[5:8])
+        temperature = th_values[0]
+        pression = th_values[1]
+
+        if temperature == -32768:
+            self.temperature = None
+        else:
+            self.temperature = float(temperature) / 10.0
+        if pression == 0xFF:
+            self.pression = None
+        else:
+            self.pression = float(pression) / 100.0
+
+    def __str__(self):
+        return 'Temperature {}, Pression {}'.format(self.temperature, self.pression)
+
+
+class PaquetTH(PaquetPayload):
+    def __init__(self, data: bytes):
+        super().__init__(data)
+        self.temperature = None
+        self.pression = None
+
+    def _parse(self):
+        super()._parse()
+        th_values = unpack('hH', self.data[5:8])
+        temperature = th_values[0]
+        humidite = th_values[1]
+
+        if temperature == -32768:
+            self.temperature = None
+        else:
+            self.temperature = float(temperature) / 10.0
+        if humidite == 0xFF:
+            self.humidite = None
+        else:
+            self.humidite = float(humidite) / 10.0
+
+    def __str__(self):
+        return 'Temperature {}, Humidite {}'.format(self.temperature, self.humidite)
+
+
+class PaquetPower(PaquetPayload):
+    def __init__(self, data: bytes):
+        super().__init__(data)
+        self.millivolt = None
+        self.reserve = None
+        self.alerte = None
+
+    def _parse(self):
+        super()._parse()
+        th_values = unpack('IBB', self.data[5:10])
+        millivolt = th_values[0]
+        reserve = th_values[1]
+        self.alerte = th_values[2]
+
+        if millivolt == 0xFFFFFFFF:
+            self.millivolt = None
+        else:
+            self.millivolt = millivolt
+        if reserve == 0xFF:
+            self.reserve = None
+        else:
+            self.reserve = reserve
+
+    def __str__(self):
+        return 'Millivolt {}, Reserve {}, Alerte {}'.format(self.millivolt, self.reserve, self.alerte)
+
+
+class MessageAppareil:
+
+    def __init__(self, paquet0: Paquet0):
+        self.__paquet0 = paquet0
+        self.__timestamp_debut = datetime.datetime.utcnow()
+
+        self.__paquets = list()
+        self.__paquets.append(paquet0)
+
+    def recevoir(self, data: bytes):
+        """
+        Recoit un nouveau paquet de payload. Retourne True quand tous les paquets sont recus.
+        :param data:
+        :return: True si tous les paquets ont ete recus
+        """
+        paquet = MessageAppareil.map(data)
+        print("Paquet: %s" % str(paquet))
+        self.__paquets.append(paquet)
+
+        if self.__paquet0.nombrePaquets == len(self.__paquets):
+            return True
+
+        return False
+
+    @staticmethod
+    def map(data: bytes):
+        type_message = data[1:3]
+
+        paquet = None
+        if type_message == 0x102:
+            paquet = PaquetTH(data)
+        elif type_message == 0x103:
+            paquet = PaquetTP(data)
+        elif type_message == 0x103:
+            paquet = PaquetPower(data)
+
+        return paquet
 
 
 while 1:
@@ -102,18 +200,15 @@ while 1:
         if chr(header.type) == 'M':
             print("Rcv {} from 0{:o}".format(unpack("h", payload)[0], header.from_node))
         elif chr(header.type) == 'P':
-            version = payload[0]
-            uuid_appareil = binascii.hexlify(payload[1:17]).decode('utf-8')
-            commande = binascii.hexlify(payload[18:20]).decode('utf-8')
-            type_nb = unpack('h', payload[20:22])
-            nombre_paquets = type_nb[0]
-
-            commande_precedente = commande
-
-            print("Rcv version:{}, UUID:{}, commande:{}, nombre_paquets:{}, from 0{:o}".format(version, uuid_appareil, commande, nombre_paquets, header.from_node))
+            fromNodeId = mesh.getNodeID(header.from_node)
+            paquet0 = Paquet0(payload)
+            message = MessageAppareil(paquet0)
+            reception_par_nodeId[fromNodeId] = message
         elif chr(header.type) == 'p':
-            routage_type_message(payload)
+            fromNodeId = mesh.getNodeID(header.from_node)
+            complet = reception_par_nodeId[fromNodeId].recevoir(payload)
+            if complet:
+                print("Message complet")
+                del reception_par_nodeId[fromNodeId]
         else:
-            print("Rcv bad type {} from 0{:o}".format(header.type,header.from_node));
-
-
+            print("Rcv bad type {} from 0{:o}".format(header.type, header.from_node));
