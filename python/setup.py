7f39e3d5d09964bcfd54953ec47fa654e440daf9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-10 23:11:22 +0000

    Ajustement versions images docker.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrillesRPi',
-    version='1.14.0',
+    version='1.14.1',
     packages=[
         'mgraspberry',
         'mgraspberry.raspberrypi'
