5d1473101725dbab4ad50c79509ba8eccb554eb7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-02-04 20:30:29 +0000

    Fix script, systemd

--- bin/mgraspberrypi.sh
+++ bin/mgraspberrypi.sh
@@ -3,6 +3,6 @@
 NB_PARAMS=${#@}
 IDMG=$1
 
-source /opt/millegrilles/etc/$IDMG/mg-noeud.conf
+source /var/opt/millegrilles/$IDMG/etc/mg-noeud.conf
 
 python3 /opt/millegrilles/bin/mgraspberrypi.py ${@:2:$NB_PARAMS}
