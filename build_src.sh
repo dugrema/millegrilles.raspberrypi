956dc99d4addd52083cb7853e30c51e3a45736be
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-10-15 18:31:36 +0000

    Maj common

--- build_src.sh
+++ build_src.sh
@@ -5,10 +5,13 @@ source image_info.txt
 
 echo "Nom build : $NAME, BRANCH=$BRANCH"
 
-mkdir -p git
+git submodule update --recursive
 
-if [ -d git/millegrilles.consignation.python ]; then
-	git -C git/millegrilles.consignation.python pull
-else
-	git -C git/ clone -b $BRANCH --single-branch ssh://docker.maceroc.com/git/millegrilles.consignation.python
-fi 
+#mkdir -p git
+#
+#if [ -d git/millegrilles.consignation.python ]; then
+#	git -C git/millegrilles.consignation.python pull
+#else
+#	git -C git/ clone -b $BRANCH --single-branch ssh://docker.maceroc.com/git/millegrilles.consignation.python
+#fi
+
